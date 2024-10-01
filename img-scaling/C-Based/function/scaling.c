cbd5cdb99c77efccae89c4cd80affc36008971d5
kibakibaki <mrkibaki@gmail.com>
2024-10-01 17:00:28 +1000

    scaling function added, testing awaits

--- img-scaling/C-Based/function/scaling.c
+++ img-scaling/C-Based/function/scaling.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <jpeglib.h>
 
-unsigned char *scaling(JSAMPLE *imgData, int orioginalHeight, int originalWidth, int pixelSize, int newWidth, int newHeight)
+JSAMPLE *scaling(JSAMPLE *imgData, int originalHeight, int originalWidth, int pixelSize, int newWidth, int newHeight)
 {
     // Nearest Neighbor Algorithm
     JSAMPLE *resizedImgData = (JSAMPLE)malloc(newWidth * newHeight * pixelSize);
@@ -12,5 +12,28 @@ unsigned char *scaling(JSAMPLE *imgData, int orioginalHeight, int originalWidth,
         exit(1);
     }
 
-    return resizedImgData;
-}
+    // calculate the ratio between new and original factrors
+    float widthRatio = (float)originalWidth / newWidth;
+    float heightRatio = (float)originalHeight / newHeight;
+
+    // Loop over the newimage
+    for (int y = 0; y < newHeight; y++)
+    {
+        for (int x = 0; x < newWidth; x++)
+        {
+            // Find corresponding position in the original image
+            int origX = (int)(x * widthRatio);
+            int origY = (int)(y * heightRatio);
+
+            // calculate posoition in the array
+            int origIndex = (originalHeight * originalWidth + origX) * pixelSize;
+            int newIndex = (y * newWidth + x) * pixelSize;
+
+            // copy pixel data
+            for (int i = 0; i < pixelSize; i++)
+            {
+                resizedImgData[newIndex + i] = imgData[origIndex + i];
+            }
+        }
+        return resizedImgData;
+    }
--- img-scaling/C-Based/main.c
+++ img-scaling/C-Based/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <jpeglib.h>
+#include "./function/scaling.h"
 
+// Driver code
 int main()
 {
     return 0;
