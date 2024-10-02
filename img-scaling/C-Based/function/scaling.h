b98fc681bc3234792b1fb6b579abce769da153b0
kibakibaki <Mrkibaki@gmail.com>
2024-10-02 14:36:32 +1000

    Delete img-scaling/C-Based directory

--- img-scaling/C-Based/function/scaling.h
+++
@@ -1,8 +0,0 @@
-#ifndef SCALING_H
-#define SCALING_H
-
-#include <jpeglib.h>
-
-JSAMPLE *scaling(JSAMPLE *imgData, int originalHeight, int originalWidth, int pixelSize, int newWidth, int newHeight);
-
-#endif
